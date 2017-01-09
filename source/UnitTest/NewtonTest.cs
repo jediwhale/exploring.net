da69b32cdd0f1b68d12c22c8327e53486d95940f
Mike Stockdale <mikes@syterra.com>
2017-01-08 17:24:41 -0700

    unit testing framework

---
+++ source/UnitTest/NewtonTest.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+namespace Exploring.UnitTest {
+    [TestFixture]
+    public class NewtonTest {
+
+        [Test]
+        public void First() {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
---
+++ source/UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest")]
+[assembly: AssemblyDescription("Unit tests for Exploring.NET")]
+[assembly: AssemblyProduct("Exploring.NET")]
+[assembly: AssemblyCopyright("Copyright © Syterra Software Inc. 2017")]
+
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.*")]
