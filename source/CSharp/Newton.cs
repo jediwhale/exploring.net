4cead2e449f46b641da5dc0de9f44a64517c4c10
Mike Stockdale <mikes@syterra.com>
2017-01-09 14:15:44 -0700

    csharp version

--- source/CSharp/Newton.cs
+++ source/CSharp/Newton.cs
@@ -1,7 +1,15 @@
-﻿namespace Exploring.Net.CSharp {
+﻿using System;
+
+namespace Exploring.Net.CSharp {
     public class Newton {
         public static double Sqrt(double input) {
-            return input;
+            if (input == 0.0) return input;
+            var guess = 1.0;
+            while (true) {
+                var newGuess = (guess + input / guess) / 2.0;
+                if (Math.Abs((guess - newGuess) / guess) < 0.001) return newGuess;
+                guess = newGuess;
+            }
         }
     }
 }
--- source/UnitTest/NewtonTest.cs
+++ source/UnitTest/NewtonTest.cs
@@ -9,5 +9,24 @@ namespace Exploring.UnitTest {
         public void InitialGuessIsCorrect() {
             Assert.AreEqual(1.0, Newton.Sqrt(1.0));
         }
+
+        [Test]
+        public void ResultGetsCloseEnough() {
+            Assert.AreEqual(2.0, Newton.Sqrt(4.0), 0.000001);
+        }
+
+        [Test]
+        public void ZeroIsHandled() {
+            Assert.AreEqual(0.0, Newton.Sqrt(0.0));
+        }
+
+        [Test]
+        public void LargeResultIsCalculated() {
+            Assert.AreEqual(1E50, Newton.Sqrt(1E100), 1E40);
+        }
+        [Test]
+        public void SmallResultIsCalculated() {
+            Assert.AreEqual(1E-50, Newton.Sqrt(1E-100), 1E-40);
+        }
     }
 }
