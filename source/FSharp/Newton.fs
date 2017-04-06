90bb41baf254332c606c8353c845bcbd01e71ab2
Mike Stockdale <mikes@syterra.com>
2017-04-06 15:13:16 -0600

    refactor extract findFixedPoint

--- source/FSharp/Newton.fs
+++ source/FSharp/Newton.fs
@@ -2,13 +2,13 @@
 
 module Newton =
 
+    let rec findFixedPoint aFunction guess =
+        let newGuess = aFunction guess
+        let goodEnough = abs((guess - newGuess) / guess) < 0.000001
+        if goodEnough then newGuess else findFixedPoint aFunction newGuess 
+
     let squareRoot input =
-
-        let rec squareRootFromGuess guess =
-            let newGuess = (guess + input / guess) / 2.0
-            let goodEnough = abs((guess - newGuess) / guess) < 0.000001
-            if goodEnough then newGuess else squareRootFromGuess newGuess 
-
+        let squareRootFromGuess = findFixedPoint (fun guess -> (guess + input / guess) / 2.0)
         match input with
             | 0.0 -> 0.0
             | input when input < 0.0 -> raise (new System.ArgumentOutOfRangeException())
